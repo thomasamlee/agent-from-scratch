00dca016c459eba0efa6fd044587c7a93f58bf16
Thomas Amlee <thomasamlee@gmail.com>
2026-01-17 17:44:24 -0600

    simple userMessage

--- src/llm.ts
+++ src/llm.ts
@@ -1,3 +1,11 @@
-import type { AIMessage } from "../types";
-import {openai} from './ai'
+import { openai } from './ai'
 
+export const runLLM = async (userMessage: string) => {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    temperature: 0.1,
+    messages: [{ role: 'user', content: userMessage }],
+  })
+
+  return response.choices[0].message.content
+}
